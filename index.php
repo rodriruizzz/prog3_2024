a3580d428693c22d447d99fec372084c8788dfb3
rodri <pradomanuelrodrigo11ruiz@gmail.com>
2024-05-15 21:24:22 -0300

    mi primer commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Primer commit</title>
+</head>
+<body>
+    <?php
+        print "Mi primera pagina";
+         
+    ?>
+</body>
+</html>
